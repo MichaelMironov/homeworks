467691d4c17da508c4168fe8a111adb60f805bd4
MichaelMironov <nemiron94@gmail.com>
2022-09-17 09:35:34 +0300

    update application class for starting calculator

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,9 +1,23 @@
+import calculator.Calculator;
+
+import java.util.Scanner;
+
 public class Application {
+
     public static void main(String[] args) {
-
-        char num = '/';
-
-        System.out.println(num);
-
+        String[] params = readParams();
+        System.out.println(Calculator.execute(params));
     }
+
+    private static String[] readParams() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите первый операнд: ");
+        String operand1 = in.nextLine();
+        System.out.print("Введите оператор: ");
+        String operator = in.nextLine();
+        System.out.print("Введите второй операнд: ");
+        String operand2 = in.nextLine();
+        return new String[]{operand1, operator, operand2};
+    }
+
 }
