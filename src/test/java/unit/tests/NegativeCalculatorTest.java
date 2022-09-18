fd7ed053a3a4dc2f310ea3499cd2601f7a410050
MichaelMironov <nemiron94@gmail.com>
2022-09-18 16:20:17 +0300

    calculator: add logger for negative tests

--- src/test/java/unit/tests/NegativeCalculatorTest.java
+++ src/test/java/unit/tests/NegativeCalculatorTest.java
@@ -4,18 +4,29 @@ import calculator.Calculator;
 import calculator.CalculatorException;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class NegativeCalculatorTest {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(NegativeCalculatorTest.class.getSimpleName());
+    private static int COUNT = 1;
+
     @ParameterizedTest
     @MethodSource("negativeData")
-    public void negativeTest(String operator, String operand1, String operand2) {
+    public void negativeTest(String operand1, String operator, String operand2) {
+
+        LOGGER.info("Тестовые данные #{}: Оператор: \"{}\". Операнды = {} и {}", COUNT++, operator, operand1, operand2);
+
         RuntimeException exception = assertThrows(CalculatorException.class, () -> {
             String[] array = {operand1, operator, operand2};
             Calculator.execute(array);
         });
+
+        LOGGER.info("Ожидаемое исключение - {}. Сообщение: {}", CalculatorException.class.getSimpleName(), exception.getMessage());
+
     }
 
     public static Object[][] negativeData() {
