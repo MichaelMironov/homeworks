784adef7cddbc2d8d37ff4aa747aad5e35af8ea0
MichaelMironov <nemiron94@gmail.com>
2022-09-18 15:32:54 +0300

    calculator: create logger for positive tests

--- src/test/java/unit/tests/PositiveCalculatorTest.java
+++ src/test/java/unit/tests/PositiveCalculatorTest.java
@@ -3,16 +3,30 @@ package unit.tests;
 import calculator.Calculator;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import web.steps.WebSteps;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PositiveCalculatorTest {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(PositiveCalculatorTest.class.getSimpleName());
+    private static int COUNT = 1;
+
     @ParameterizedTest
     @MethodSource("positiveData")
     public void positiveTest(String operand1, String operator, String operand2, double expectedResult) {
         String[] array = {operand1, operator, operand2};
-        assertEquals(Double.parseDouble(Calculator.execute(array)), expectedResult, 0.001);
+
+        LOGGER.info("Тестовые данные #{}: Оператор: \"{}\". Операнды = {} и {}" , COUNT++ ,operator, operand1, operand2);
+
+        double actualResult = Double.parseDouble(Calculator.execute(array));
+
+        LOGGER.info("Ожидаемый результат = {}. Фактический результат = {}", expectedResult, actualResult);
+
+        assertEquals(expectedResult, actualResult, 0.001);
+
     }
 
     public static Object[][] positiveData() {
