fd97ecfdba16e63c2923ca7e3e09f4b994377109
MichaelMironov <nemiron94@gmail.com>
2022-09-20 22:38:04 +0300

    create tasks page

--- src/test/java/web/ru/jira/pages/TasksPage.java
+++ src/test/java/web/ru/jira/pages/TasksPage.java
@@ -1,2 +1,19 @@
-package web.ru.jira.pages;public class TasksPage {
+package web.ru.jira.pages;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$x;
+
+public class TasksPage {
+
+    public static SelenideElement filter = $x("//div[@class='order-by-fields']");
+
+    public  static SelenideElement filterPopup = $x("//a[contains(@class, 'active')]");
+
+    public static SelenideElement filterText = $x("//input[@aria-label=\"Поиск\"]");
+
+    public static SelenideElement searcher = $x("//input[@id=\"searcher-query\"]");
+
+    public static SelenideElement tasksCount = $x("//div[@class='showing']//child::span");
+
 }
