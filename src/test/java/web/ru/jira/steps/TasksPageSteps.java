4998b03b49916d0325e537c1f93a4cbe953cd357
MichaelMironov <nemiron94@gmail.com>
2022-09-20 22:38:41 +0300

    create tasks page steps

--- src/test/java/web/ru/jira/steps/TasksPageSteps.java
+++ src/test/java/web/ru/jira/steps/TasksPageSteps.java
@@ -1,2 +1,30 @@
-package web.ru.jira.steps;public class TasksPageSteps {
+package web.ru.jira.steps;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.commands.PressEnter;
+
+import static web.ru.jira.pages.TasksPage.*;
+
+public class TasksPageSteps {
+
+//    public static void filterByText(String text){
+//        filter.click();
+//        filterPopup.shouldBe(Condition.visible);
+//        filterText.click();
+//        filterText.sendKeys(text);
+//    }
+
+    public static void searchByText(String text){
+        searcher.click();
+        searcher.shouldHave(Condition.attribute("data-focus-visible-added")).sendKeys(text);
+        searcher.pressEnter();
+    }
+
+    public static void totalCountTasks(){
+       String[] temp = tasksCount.innerText().split(" ");
+
+       //TODO by logger
+        System.out.println("Общее количество задач: " + temp[temp.length-1]);
+    }
+
 }
