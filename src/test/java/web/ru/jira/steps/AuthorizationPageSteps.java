9e56382cd23c71dae373963b67167c5120311d56
MichaelMironov <nemiron94@gmail.com>
2022-09-20 21:22:11 +0300

    create authorization page steps

--- src/test/java/web/ru/jira/steps/AuthorizationPageSteps.java
+++ src/test/java/web/ru/jira/steps/AuthorizationPageSteps.java
@@ -1,9 +1,36 @@
-package web.steps;
+package web.ru.jira.steps;
 
+import com.codeborne.selenide.Condition;
 import io.qameta.allure.Step;
+import org.junit.jupiter.api.Test;
+import web.ru.jira.pages.AuthorizationPage;
 
-public class WebActions {
+import static com.codeborne.selenide.Selenide.open;
+import static utils.Configuration.getConfigurationValue;
+import static web.ru.jira.pages.AuthorizationPage.*;
 
-    @Step("Авторизоваться в системе под пользователем {}")
-    public static void 
+public class AuthorizationPageSteps {
+
+    public static final String USER = "user";
+
+    @Step("Открыть страницу авторизации")
+    public static void openAuthorizationPage(){
+        open(getConfigurationValue("authorizeUrl"));
+    }
+
+    @Step("Авторизоваться в системе как {login}")
+    public static void logInAs(String login){
+
+        /*  При проверке инпутов, тесты на видимость и фокус проходят вне зависимости от того аткивен инпут или нет
+        На активное поле ввода вешается кастомный аттрибут: data-focus-visible-added    */
+
+        loginField.click();
+        loginField.shouldHave(Condition.attribute("data-focus-visible-added")).sendKeys(getConfigurationValue(login));
+
+        passwordField.click();
+        passwordField.shouldHave(Condition.attribute("data-focus-visible-added")).sendKeys(getConfigurationValue("password"));
+
+        buttonLogIn.shouldBe(Condition.enabled).click();
+
+    }
 }
