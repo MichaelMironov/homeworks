8631c142109e5261ac0b421cc4db3954622ecf3c
MichaelMironov <nemiron94@gmail.com>
2022-09-21 13:11:49 +0300

    create method for opening task by id

--- src/test/java/web/ru/jira/steps/TasksPageSteps.java
+++ src/test/java/web/ru/jira/steps/TasksPageSteps.java
@@ -1,30 +1,45 @@
 package web.ru.jira.steps;
 
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.commands.PressEnter;
+import io.qameta.allure.Step;
 
+import java.util.Arrays;
+
+import static com.codeborne.selenide.Selenide.$x;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static web.ru.jira.pages.TasksPage.*;
 
 public class TasksPageSteps {
 
-//    public static void filterByText(String text){
-//        filter.click();
-//        filterPopup.shouldBe(Condition.visible);
-//        filterText.click();
-//        filterText.sendKeys(text);
-//    }
-
-    public static void searchByText(String text){
+    @Step("Отфильтровать задачи по тексту: {text}")
+    public static void filterTasksByText(String text) {
         searcher.click();
         searcher.shouldHave(Condition.attribute("data-focus-visible-added")).sendKeys(text);
         searcher.pressEnter();
     }
 
-    public static void totalCountTasks(){
-       String[] temp = tasksCount.innerText().split(" ");
+    @Step("Общее количество задач")
+    public static void totalCountTasks() {
+        String[] temp = tasksCount.innerText().split(" ");
 
-       //TODO by logger
-        System.out.println("Общее количество задач: " + temp[temp.length-1]);
+        //TODO by logger
+        System.out.println("Общее количество задач: " + temp[temp.length - 1]);
     }
+
+    @Step("Открыть задачу с id: {id}")
+    public static void openTaskWithId(int id) {
+        $x("//li[contains(@data-key, 'TEST-" + id + "')]").shouldBe(Condition.visible).click();
+    }
+
+    @Step("Проверка исправления в версии: {expectedVersion}")
+    public static void checkFixIn(String expectedVersion) {
+
+        String actualVersion = Arrays.stream($x("//span[@id=\"fixfor-val\"]//a")
+                        .innerText().split(" "))
+                        .reduce((first, second) -> second).get();
+
+        assertEquals(expectedVersion, actualVersion);
+    }
+
 
 }
