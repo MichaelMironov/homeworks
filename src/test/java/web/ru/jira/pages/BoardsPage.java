2a4c201ccdcc39daa52440ec5402a991d69996ed
MichaelMironov <nemiron94@gmail.com>
2022-09-22 00:36:34 +0300

    add field for active tasks

--- src/test/java/web/ru/jira/pages/BoardsPage.java
+++ src/test/java/web/ru/jira/pages/BoardsPage.java
@@ -1,2 +1,20 @@
-package web.ru.jira.pages;public class BoardsPage {
+package web.ru.jira.pages;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$x;
+
+public class BoardsPage {
+
+    public static SelenideElement backLog = $x("//div[@class='ghx-name']");
+
+    public static SelenideElement searcherBacklogTasks = $x("//input[@aria-label=\"Поиск задач\"]");
+
+    public static SelenideElement confirmButton = $x("//button[contains(@class,'button-panel-button')]");
+
+    public static SelenideElement tasksList = $x("//a[starts-with(@class, 'aui-nav-item ')]");
+
+    public static SelenideElement activeTasks = $x("//span[@title='Активные спринты']");
+
+
 }
