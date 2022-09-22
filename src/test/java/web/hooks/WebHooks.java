c42581b2278ba2dc81e86ed0b5c139d97bdc4e10
MichaelMironov <nemiron94@gmail.com>
2022-09-22 09:25:33 +0300

    create web hooks class

--- src/test/java/web/hooks/WebHooks.java
+++ src/test/java/web/hooks/WebHooks.java
@@ -1,2 +1,27 @@
-package web.hooks;public class WebHooks {
+package web.hooks;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+import static utils.Configuration.getConfigurationValue;
+
+public class WebHooks {
+    @BeforeAll
+    public static void authorizeAsUser() {
+
+        Configuration.timeout = 7000;
+        Configuration.startMaximized = true;
+
+        String webDriverLocation = null;
+
+        if (System.getProperty("os.name").toUpperCase().contains("MAC"))
+            webDriverLocation = getConfigurationValue("webdriverLocalMac");
+        if (System.getProperty("os.name").toUpperCase().contains("WINDOWS"))
+            webDriverLocation = getConfigurationValue("webdriverLocalWin");
+
+        if (webDriverLocation != null) {
+            System.setProperty("webdriver.chrome.driver", webDriverLocation);
+        }
+
+    }
 }
