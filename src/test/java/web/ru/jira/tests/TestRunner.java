2400ac9235272dedf302682c65a27511d871ec54
Michael <nemiton94@gmail.com>
2022-09-24 14:35:16 +0300

    create test runner

--- src/test/java/web/ru/jira/tests/TestRunner.java
+++ src/test/java/web/ru/jira/tests/TestRunner.java
@@ -6,7 +6,8 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"}, features = "classpath:features")
-public class Runner {
+@CucumberOptions(plugin = {"pretty"}, features = "src/test/resources/features/jira-e2e.feature")
+public class TestRunner {
+
 }
 
