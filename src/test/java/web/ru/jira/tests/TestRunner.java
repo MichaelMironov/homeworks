3ca82994c7a469f40ae7bbb7b07602637af1a376
MichaelMironov <nemiron94@gmail.com>
2022-09-24 18:09:08 +0300

    add cucumber options

--- src/test/java/web/ru/jira/tests/TestRunner.java
+++ src/test/java/web/ru/jira/tests/TestRunner.java
@@ -4,10 +4,11 @@ package web.ru.jira.tests;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
+import web.hooks.WebHooks;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"}, features = "src/test/resources/features/jira-e2e.feature")
-public class TestRunner {
+@CucumberOptions(plugin = {"pretty"}, glue = {"steps", "hooks"}, tags = "@jira", features = "classpath:features")
+public class TestRunner extends WebHooks {
 
 }
 
