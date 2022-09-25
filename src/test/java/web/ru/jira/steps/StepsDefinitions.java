e773f26528e4fb2afb3fc42375c2c2f0ec16dd80
MichaelMironov <nemiron94@gmail.com>
2022-09-25 23:40:48 +0300

    added column in_work for moving task method

--- src/test/java/web/ru/jira/steps/StepsDefinitions.java
+++ src/test/java/web/ru/jira/steps/StepsDefinitions.java
@@ -5,7 +5,6 @@ import io.cucumber.java.ru.Дано;
 import io.cucumber.java.ru.Затем;
 import io.cucumber.java.ru.И;
 import io.cucumber.java.ru.Тогда;
-import web.hooks.WebHooks;
 import web.ru.jira.models.Task;
 
 import static web.ru.jira.elements.NavigationPanel.*;
@@ -19,50 +18,56 @@ import static web.ru.jira.steps.TasksPageSteps.*;
 public class StepsDefinitions {
 
     @Дано("^пользователь авторизован в системе как ([^\"]*)$")
-    public static void authorizeAs(String user){
+    public static void authorizeAs(String user) {
         openAuthorizationPage();
         logInAs(user);
     }
 
     @Тогда("^в разделе ([^\"]*) выбрать подраздел ([^\"]*)$")
-    public static void chooseSubMenu(String menu, String submenu){
+    public static void chooseSubMenu(String menu, String submenu) {
         SelenideElement elem = null;
 
-        switch (menu){
-            case "Задачи": elem = TASKS;
+        switch (menu) {
+            case "Задачи":
+                elem = TASKS;
         }
 
         selectMenuSubsectionByText(elem, submenu);
     }
 
     @И("^проверить общее количество заведенных задач в проекте$")
-    public static void tasksCount(){
+    public static void tasksCount() {
         totalCountTasks();
     }
 
     @Затем("^перейти в задачу с названием - ([^\"]*) и id - (\\d+)$")
-    public static void goToTask(String title, int id){
+    public static void goToTask(String title, int id) {
         filterTasksByText(title);
         openTaskWithId(id);
     }
 
     @И("^статус задачи - ([^\"]*), исправить в версиях - ([^\"]*)$")
-    public static void checkTask(String status, String version){
+    public static void checkTask(String status, String version) {
         statusShouldBe(status);
         checkFixIn(version);
     }
 
     @Затем("^создать новую задачу с типом ([^\"]*), именем ([^\"]*) и описанием ([^\"]*)$")
-    public static void createTask(String type, String title, String description){
+    public static void createTask(String type, String title, String description) {
         clickToCreateTask();
         newTask = createNewTask(title, type, description);
     }
 
     @И("^перенести новую задачу на скрам-доске в колонку ([^\"]*)$")
-    public static void movingTo(String column){
+    public static void movingTo(String column) {
 
-        switch (column){
-            case "Выполнено": column = DONE;
+        switch (column) {
+            case "Выполнено":
+                column = DONE;
+                break;
+            case "В работе":
+                column = IN_WORK;
+                break;
         }
 
         selectMenuSubsectionByText(TASKS, "Поиск задач");
@@ -78,7 +83,8 @@ public class StepsDefinitions {
         toSprintBoard();
 
         moveTaskByIdTo(newTask.getId(), IN_WORK);
-        moveTaskByIdTo(newTask.getId(), column);
+        if (column.equalsIgnoreCase(DONE))
+            moveTaskByIdTo(newTask.getId(), column);
     }
 
     static Task newTask;
