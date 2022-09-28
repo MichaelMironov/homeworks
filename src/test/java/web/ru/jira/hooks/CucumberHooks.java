6646764734984b5176c92a984ae0b50e17c11eeb
MichaelMironov <nemiron94@gmail.com>
2022-09-28 20:06:31 +0300

    added loggers for starting/finishing scenarios

--- src/test/java/web/ru/jira/hooks/CucumberHooks.java
+++ src/test/java/web/ru/jira/hooks/CucumberHooks.java
@@ -2,20 +2,21 @@ package web.ru.jira.hooks;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 
 import static web.ru.jira.hooks.WebHooks.setConfiguration;
 import static web.ru.jira.steps.AuthorizationPageSteps.logOut;
-import static web.ru.jira.steps.BoardsPageSteps.deleteSuccessfulTask;
 
 public class CucumberHooks {
     @Before()
-    public static void setUp() {
+    public void setUp(Scenario scenario) {
+        scenario.log("Начало сценария: " + scenario.getName());
         setConfiguration();
     }
 
     @After
-    public static void postCondition() {
-        deleteSuccessfulTask(false);
+    public void postCondition(Scenario scenario) {
+        scenario.log("Окончание сценария: " + scenario.getName());
         logOut();
     }
 }
