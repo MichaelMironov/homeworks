4b04425d17451454b9a9dae553222043e90c1f1a
MichaelMironov <nemiron94@gmail.com>
2022-09-30 11:55:58 +0300

    upd cucumber options

--- src/test/java/web/ru/jira/tests/RunnerTest.java
+++ src/test/java/web/ru/jira/tests/RunnerTest.java
@@ -9,7 +9,8 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         glue = {"steps", "hooks"},
         features = "classpath:features",
-        plugin = {"pretty"})
+        plugin = {"pretty"},
+        tags = "@jira")
 public class RunnerTest {
 
 }
