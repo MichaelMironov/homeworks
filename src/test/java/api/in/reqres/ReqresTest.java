f20725dbef745a0220dda76f323a08f4f6147418
MichaelMironov <nemiron94@gmail.com>
2022-10-04 22:00:23 +0300

    created tests for reqres

--- src/test/java/api/in/reqres/ReqresTest.java
+++ src/test/java/api/in/reqres/ReqresTest.java
@@ -1,2 +1,72 @@
-package api.in.reqres;public class ReqresTest {
+package api.in.reqres;
+
+import api.com.rickandmortyapi.Specification;
+import api.in.reqres.pojo.Potato;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.junit.jupiter.api.*;
+import java.io.File;
+import java.io.IOException;
+
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class ReqresTest {
+
+    private static String ID;
+    private static Potato potato;
+
+    @BeforeAll
+    public static void prepare() throws IOException {
+
+        System.getProperties().load(ClassLoader.getSystemResourceAsStream("test.properties"));
+        String baseUri = System.getProperty("reqresUrl");
+        if (baseUri == null || baseUri.isEmpty()) {
+            throw new RuntimeException("В файле \"test.properties\" отсутствует значение \"reqresUrl\"");
+        }
+        baseURI = baseUri;
+        Specification.installSpecification(Specification.requestSpec(baseURI));
+
+    }
+
+    @Order(1)
+    @Test
+    void creatingTest() throws IOException {
+
+        ObjectNode json = new ObjectMapper().readValue(new File("src/test/resources/json/potato.json"), ObjectNode.class);
+
+        potato =
+                given()
+                        .body(json)
+                        .basePath("/users")
+                        .when().post()
+                        .then()
+                        .statusCode(201)
+                        .log().body()
+                        .extract().body().as(Potato.class);
+
+        ID = potato.getId();
+
+    }
+
+    @Order(2)
+    @Test
+    void changingTest() {
+
+        potato.setName("Tomato");
+        potato.setJob("Eat Maket");
+
+        given()
+                .body(potato)
+                .basePath("/users/" + ID)
+                .when().put()
+                .then().statusCode(200)
+                .body("name", equalTo("Tomato"))
+                .body("job", equalTo("Eat Maket"))
+                .body("id", equalTo(ID));
+
+    }
+
 }
