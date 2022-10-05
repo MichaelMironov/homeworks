b452999015d7f726673477e440c3f8bc48edf941
MichaelMironov <nemiron94@gmail.com>
2022-10-05 12:42:40 +0300

    added test for jira

--- src/test/java/api/ru/jira/JiraTest.java
+++ src/test/java/api/ru/jira/JiraTest.java
@@ -1,2 +1,45 @@
-package api.ru.jira;public class JiraApiTest {
+package api.ru.jira;
+
+import io.restassured.response.Response;
+import utils.api.Specification;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static api.ru.jira.Steps.getSessionId;
+import static io.restassured.RestAssured.*;
+import static java.util.Optional.empty;
+import static org.hamcrest.core.IsNot.not;
+import static utils.Configuration.getConfigurationValue;
+
+public class JiraTest {
+
+    public static String sessionID;
+    public static final String BOARDS = getConfigurationValue("jiraBoardsEndpoint");
+
+    @BeforeAll
+    static void prepare() {
+
+        baseURI = getConfigurationValue("jiraUrl");
+
+        Specification.installSpecification(Specification.requestSpec(baseURI));
+
+        sessionID = getSessionId(getConfigurationValue("user"), getConfigurationValue("password"), "JSESSIONID");
+
+        Specification.installSpecification(Specification.requestSpecWithSession(baseURI, sessionID));
+
+    }
+
+    @Test
+    void getTasksList() {
+        Response tasksList =
+                (Response)
+                        when()
+                                .get(BOARDS)
+                                .then()
+                                .body("results", not(empty()))
+                                .extract().body();
+
+
+        tasksList.getBody().prettyPrint();
+    }
 }
--- src/test/java/api/ru/jira/Steps.java
+++ src/test/java/api/ru/jira/Steps.java
@@ -1,2 +1,15 @@
-package api.ru.jira;public class Steps {
+package api.ru.jira;
+
+import static io.restassured.RestAssured.given;
+
+public class Steps {
+
+    public static String getSessionId(String login, String password, String cookie){
+        return given()
+                .auth()
+                .preemptive().basic(login, password)
+                .when().post()
+                .then().log().body()
+                .extract().cookie(cookie);
+    }
 }
