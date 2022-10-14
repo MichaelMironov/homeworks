28a8d0497efc1b1da1f42cd9c9973ca8d3a0bd6e
MichaelMironov <nemiron94@gmail.com>
2022-10-14 09:28:43 +0300

    created interface for web configuration

--- src/main/java/utils/configurations/WebConfiguration.java
+++ src/main/java/utils/configurations/WebConfiguration.java
@@ -1,2 +1,21 @@
-package utils.configurations;public class WebConfiguration {
+package utils.configurations;
+
+import org.aeonbits.owner.Config;
+
+@Config.LoadPolicy(Config.LoadType.MERGE)
+@Config.Sources({
+        "classpath:config/chrome.properties",
+        "system:properties",
+        "system:env"
+})
+public interface WebConfiguration extends Config {
+
+    @Key("webdriver.browser.size")
+    @DefaultValue("1920x1080")
+    String webDriverBrowserSize();
+
+    @Key("webdriver.timeoutSeconds")
+    @DefaultValue("5")
+    int webDriverTimeoutSeconds();
+
 }
